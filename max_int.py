57fd005c3c29c2dee4307b410faf5f8d55670a33
Margret Gudmundsdottir <margret@margud.com>
2019-09-05 09:51:16 +0000

    Program written for max_int

--- max_int.py
+++ max_int.py
@@ -2,6 +2,17 @@
 
 # 1. Prompt the user for a positive integer input.
 # 2. Prompt the user for repeated inputs until a negative value is entered.
-# 3. If the new value is higher than the value before, log it as the highest value
+# 3. If the new value is higher than the value before, log as the highest value
 # 4. Repeat until a negative value is entered
 # 5. Print the maximum positive integer
+
+
+num_int = int(input("Input a number: "))
+max_int = num_int
+
+while num_int > 0:
+    num_int = int(input("Input a number: "))
+    if num_int > max_int:
+        max_int = num_int
+
+print("The maximum is", max_int)
